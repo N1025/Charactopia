11e19ddc739711cf885455b87fedc7312de927f9
N1025 <narashi1025@gmail.com>
2019-10-11 21:37:10 +0900

    使用開始

---
+++ Assets/スクリプト/EnemyDataBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyDataBase", menuName = "CreateEnemyDataBase")]
+public class EnemyDataBase : ScriptableObject
+{
+    [SerializeField]
+    private List<Enemy> enemyLists = new List<Enemy>();
+
+    //アイテムリストを返す
+    public List<Enemy> GetEnemyLists()
+    {
+        return enemyLists;
+    }
+
+}
---
+++ Assets/スクリプト/PlayerDataBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerDataBase", menuName = "CreatePlayerDataBase")]
+public class PlayerDataBase : ScriptableObject
+{
+    [SerializeField]
+    private List<Player> playerLists = new List<Player>();
+
+    //アイテムリストを返す
+    public List<Player> GetPlayerLists()
+    {
+        return playerLists;
+    }
+
+}
+
